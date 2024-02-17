7aefb3e0d8f6daf83bbbf9c1b70379ebd8ab78d7
del <gsedubun@gmail.com>
2024-02-18 00:12:46 +0700

    addsalesperson to district tests

--- Sd-Solution.Tests/SalesPersonControllerTests.cs
+++ Sd-Solution.Tests/SalesPersonControllerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using WebapiSales.Controllers;
 using WebapiSales.DataAccess.Interfaces;
+using WebapiSales.DataAccess.Models;
 using WebapiSales.DataAccess.ViewModels;
 
 namespace Sd_Solution.Tests
@@ -37,6 +38,40 @@ namespace Sd_Solution.Tests
             
             Assert.IsType<NotFoundObjectResult>(result);
 
+        }
+
+        [Fact]
+        public void AddSalesPersonToDistrict_ReturnOk()
+        {
+            var salesPerson = new AddSalesPersonToDistrictViewModel
+            {
+                DistrictId = 1,
+                SalesPersonId = 1,
+                SalesType = "Primary"
+            };
+            _mockDistrictRepository.Setup(x => x.GetDistrict(salesPerson.DistrictId)).Returns(new DistrictViewModel(){ DistrictId = 1});
+           
+            var result = _controller.AddSalesPersonToDistrict(salesPerson);
+            
+            Assert.IsType<OkObjectResult>(result);
+            _mockDistrictRepository.Verify(x => x.UpdateDistrict(It.IsAny<District>()), Times.Once);
+        }
+
+        [Fact]
+        public void AddSalesPersonToDistrict_Secondary_ReturnOk()
+        {
+            var salesPerson = new AddSalesPersonToDistrictViewModel
+            {
+                DistrictId = 1,
+                SalesPersonId = 1,
+                SalesType = "Secondary"
+            };
+            _mockDistrictRepository.Setup(x => x.GetDistrict(salesPerson.DistrictId)).Returns(new DistrictViewModel() { DistrictId = 1});
+            
+            var result = _controller.AddSalesPersonToDistrict(salesPerson);
+            
+            Assert.IsType<OkObjectResult>(result);
+            _mockSecondarySalesRepository.Verify(x => x.AddSecondarySalesPerson(It.IsAny<SecondarySalesPerson>()), Times.Once);
         }
     }
 }
